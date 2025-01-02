fdcd49322f615307d54598997935121bb2ff44e8
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-02 13:24:50 +0530

    Array length

--- array.sh
+++ array.sh
@@ -1,4 +1,7 @@
 # array : Array is a data structure with contiguous memory allocation. They store multiple type of data in a single variable.
 myArray=(1 2 3 4 5 a b c d e true false 22.02 190720242034) # Array Decleraion
-echo "My array looks like: ${myArray[10]}"
+echo "array looks like : ${myArray[@]}" #priting all the elements of the array
+echo "My array looks like: ${myArray[10]}" # printing the 10th index element
+# Array length
+echo "Array Length is ${#myArray[@]}"
 
