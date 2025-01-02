551f4f63deaec728b485d7fe6e5ce6c16476d649
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-02 12:18:15 +0530

    Array Decleration

---
+++ array.sh
@@ -0,0 +1,4 @@
+# array : Array is a data structure with contiguous memory allocation. They store multiple type of data in a single variable.
+myArray=(1 2 3 4 5 a b c d e true false 22.02 190720242034) # Array Decleraion
+echo "My array looks like: ${myArray[10]}"
+
