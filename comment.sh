3594aa85c50c300877b8d4c6593f933780d565fb
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-02 12:02:51 +0530

    upload comment file

---
+++ comment.sh
@@ -0,0 +1,6 @@
+# This is a  single line comment. It is basically use for single line documentation.
+# bash is not support the multi line comments, but if we want to comment multiple line we use " <<  "
+<<comment
+this is line 1 for comment
+this is line2 for comment
+comment
