27d7f43bf7084abde8ee0fdc5b009dc95e4de5d8
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-07 18:05:22 +0530

    Update or Adding another value in the existing array and also access

--- associated_array.sh
+++ associated_array.sh
@@ -6,3 +6,6 @@ echo "${myarr[id]}"
 declare -A myarray #we need to declare associated array without declaring its not working
 myarray=(["name"]="Nitin" ["age"]=24 ["city"]="Banglore")
 echo "Multiple key Value pair at a time name : ${myarray["name"]}"
+# update Array- means we add another key-value pair in the array
+myarr+=([city]=India)
+echo "Candidate city is :${myarr[city]}"
