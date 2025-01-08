ec98158d6bcecc5c84fee15c702c6e95397f7a22
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-08 12:32:55 +0530

    Fetch Sub-string

--- string.sh
+++ string.sh
@@ -7,3 +7,7 @@ echo "the length of the string is : ${#str}"
 #Replace the string (whole-string/sub-string)
 repl=${str/world/word in bash scripting }
 echo "After replacing the string : ${repl}"
+#Extract the sub string in the main string
+substr=${str:2:6} #include index (2) and count (6) character include 'space'
+echo "The sub string is : ${substr}"
+
