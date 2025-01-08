f7ddc0afbcf4079391d2d599fd5f31510d78ed5a
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-08 12:28:56 +0530

    Replace the string

--- string.sh
+++ string.sh
@@ -4,3 +4,6 @@ str='Hello world'
 echo "The string is : ${str}"
 #Finding the length of the string
 echo "the length of the string is : ${#str}"
+#Replace the string (whole-string/sub-string)
+repl=${str/world/word in bash scripting }
+echo "After replacing the string : ${repl}"
