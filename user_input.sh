306d18f7735a964f6170a55a18fbd8b6df4c3a10
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-11 12:42:40 +0530

    User Input

---
+++ user_input.sh
@@ -0,0 +1,7 @@
+#Taking user input
+#without prompt
+read name
+echo "Your name is : $name"
+#With prompt
+read -p "Enter your name :" NAME #-p is stand for prompt
+echo "Your entered your name is : $NAME"
