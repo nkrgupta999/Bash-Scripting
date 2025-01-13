f94e014832401ed052f8b22f2fa62715f945483d
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-13 20:11:24 +0530

    Using let keyword

--- operator.sh
+++ operator.sh
@@ -1,5 +1,11 @@
 #Operator
 #airthmatic operator
 #1. using let keyword
-let a++
+let a++ #Increment of value
 echo "value of a: $a"
+let a-- #Decrement of value
+echo "Value of a after decrement : $a"
+let a=5*a #Multiplication
+echo "value after multiplication: $a"
+let a=a/100
+echo "Value after division: $a"
