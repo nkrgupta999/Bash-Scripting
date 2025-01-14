8d7a87be89258be63db4bb093c3b9486acc30ab4
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-14 19:01:20 +0530

    if-Statement

--- conditional_statement.sh
+++ conditional_statement.sh
@@ -3,10 +3,12 @@
 a=10
 b=100
 #1. if-statement
+echo "If condition:"
 if [ $a -lt $b ];then # Space is very important after and before brackets '[]'
 	echo "The value of a is less than value of b"
 fi
 #2. if-else statement
+echo ""
 if [ $a -gt $b ];then
 	echo "The value of a is greater than value of b "
 else
