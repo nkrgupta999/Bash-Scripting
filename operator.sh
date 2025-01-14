bd7a0e43c340e2d84ba05200c1de252b387074c1
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-14 18:45:28 +0530

    Using (()) symbol for airthmatic operation and expressio also

--- operator.sh
+++ operator.sh
@@ -1,5 +1,5 @@
 #Operator
-#airthmatic operator
+#A. Airthmatic operator
 #1. using let keyword
 let a++ #Increment of value
 echo "value of a: $a"
@@ -9,3 +9,15 @@ let a=5*a #Multiplication
 echo "value after multiplication: $a"
 let a=a/100
 echo "Value after division: $a"
+echo "-----------------------------------"
+#2. using (()) bracket symbol
+((a++)) #simple Airthmatic operation
+echo "The starting value of a is  : $a"
+exprs=$((2+a*(10-5))) #expersion operation
+echo "The value of a after expression : $a"
+echo "The value of expression: $exprs"
+
+#B. comparision Operator
+#1. Equal to : '-eq','=='
+echo "-----------------------------------"
+
