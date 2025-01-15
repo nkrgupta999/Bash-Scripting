d7fa334cbb3bd6f110a23330ef867817eb4dd98f
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-15 12:17:38 +0530

    Add while looping statement

--- looping_statement.sh
+++ looping_statement.sh
@@ -10,4 +10,24 @@ for a in {1..9};
 do
 	echo "Value is: $a"
 done
+echo "--------------------------------------- "
+#3. while loop
+ar=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+i=0
+while [ $i -lt ${#ar[@]} ]; do #-lt is stands for less than 
+    if [ "${ar[$i]}" -ge 11 ]; then #-ge is stand for greater than and equal to 
+        echo "Value is: ${ar[$i]}"
+    fi
+    ((i++))  # Increment the index
+done
+echo "-----------------------------------------"
+#4. until loop
+count=1
+until [$count -gt 10];
+do
+	echo "The Value is: $count"
+	(($count++))
+done
+echo "-----------------------------------------"
+
 
