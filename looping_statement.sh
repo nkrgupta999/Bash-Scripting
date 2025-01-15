781a99999cff7cc2d01cf712eb1492fb4b7bc12f
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-15 12:02:55 +0530

    Add for Looping statement (with array and Range)

--- looping_statement.sh
+++ looping_statement.sh
@@ -1,8 +1,13 @@
 #Looping statement
-#1. for loop
+#1. for loop with a specific array define
 arr=('Apple' 'Mango' 'Banana' 1 2 3 4 5 )
 for i in ${arr[@]}; do
 	echo "value is: $i"
 done
-
+echo "---------------------------------------"
+#2. for loop with a range
+for a in {1..9};
+do
+	echo "Value is: $a"
+done
 
