5214771d95823e65e429dc6a106bc30287961b0d
nkrgupta999 <nitinkumargupta999@outlook.com>
2025-01-17 21:07:41 +0530

    Function declaration and function calling

---
+++ functions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Function
+#Function decleration
+#function_name () {
+	#body of the function or task or commands
+#	echo "Sample function"
+#}
+
+# Function calling
+# function_name #-----------we not need any () brackets we simply calling by function name.
+#1. Basic function
+greet () {
+	echo "Hello from greet function in bash Scripting"
+}
+greet	#function calling
